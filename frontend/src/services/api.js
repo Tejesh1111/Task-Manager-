8d90bb303d465cb675a78bee8dbb948c6a2881f2
Tejesh1111 <gaddetejesh@gmail.com>
2026-04-14 21:01:43 +0530

    Connected Frontend to backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1"
+  baseURL: "https://task-manager-backend-nfgy.onrender.com/api/v1"
 });
 
 // Attach token automatically
