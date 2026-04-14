1402955181143b4d97b5ffd1dc0399479b835bd0
Tejesh1111 <gaddetejesh@gmail.com>
2026-04-14 19:43:07 +0530

    modified database for Deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,20 +4,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  uri: process.env.MYSQL_URL
 });
 
-// Test connection
 db.getConnection((err, connection) => {
-    if (err) {
-        console.log("DB connection failed:", err.message);
-    } else {
-        console.log("MySQL Connected...");
-        connection.release();
-    }
+  if (err) {
+    console.log("DB connection failed:", err.message);
+  } else {
+    console.log("MySQL Connected...");
+    connection.release();
+  }
 });
 
 module.exports = db;
